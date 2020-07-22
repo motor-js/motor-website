5022b2a5ac7a72f1a08c7dafb0cecfadd1072fdb
Alan Green <Alan_Green@Alans-MacBook-Pro.local>
2020-07-22 13:29:46 +0100

    Contact form continued

--- src/components/ContactUsForm.jsx
+++ src/components/ContactUsForm.jsx
@@ -46,16 +46,12 @@ const Modal = ({
                 data-version="5"
                 data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"facebook":null,"segment":null,"pinterest":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":false,"url":"https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form"},"recaptcha":{"enabled":true},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
                 min-width="400 500 600 700 800"
-                style={{
-                  backgroundColor: `rgb(255, 255, 255)`,
-                  borderRadius: `8px`,
-                }}
               >
                 <div data-style="card">
                   <div
                     data-element="column"
                     className="formkit-column"
-                    style={{ backgroundColor: `rgb(251, 105, 112)` }}
+                    style={{ backgroundColor: `var(--redBrand)` }}
                   >
                     <div className="formkit-background"></div>
                     <div className="formkit-header" data-element="header">
@@ -143,14 +139,55 @@ const Modal = ({
                             save_as="Tag"
                             group="field"
                           >
-                            <legend
-                              style={{
-                                color: `rgb(77, 77, 77)`,
-                                fontWeight: 400,
-                              }}
+                            <legend>Reason for your enquiry :</legend>
+                            <div
+                              className="formkit-checkboxes"
+                              data-element="tags-checkboxes"
+                              data-group="checkbox"
                             >
-                              Reason for your enquiry :
-                            </legend>
+                              <input
+                                className="formkit-checkbox"
+                                id="tag-4184380-1743428"
+                                type="checkbox"
+                                name="tags[]"
+                                value="1743428"
+                              />
+                              <label htmlFor="tag-4184380-1743428">
+                                Enterprise license query
+                              </label>
+                            </div>
+                            <div
+                              className="formkit-checkboxes"
+                              data-element="tags-checkboxes"
+                              data-group="checkbox"
+                            >
+                              <input
+                                className="formkit-checkbox"
+                                id="tag-4184380-1743428"
+                                type="checkbox"
+                                name="tags[]"
+                                value="1743428"
+                              />
+                              <label htmlFor="tag-4184380-1743428">
+                                Enterprise license query
+                              </label>
+                            </div>
+                            <div
+                              className="formkit-checkboxes"
+                              data-element="tags-checkboxes"
+                              data-group="checkbox"
+                            >
+                              <input
+                                className="formkit-checkbox"
+                                id="tag-4184380-1743428"
+                                type="checkbox"
+                                name="tags[]"
+                                value="1743428"
+                              />
+                              <label htmlFor="tag-4184380-1743428">
+                                Enterprise license query
+                              </label>
+                            </div>
                             <div
                               className="formkit-checkboxes"
                               data-element="tags-checkboxes"
