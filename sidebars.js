9e0bbf082a8b63d2349c1109fa99527ff31fa32b
lureilly1 <lureilly1@gmail.com>
2020-08-17 22:37:16 +0200

    customised sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,10 @@
 module.exports = {
-  someSidebar: {
-    Motor: ['Introduction', 'Getting Started','Theming', 'Colors'],
-    Contexts: ['Motor'],
-    Visualisations: ['Column', 'Bar', 'Line', 'Pie', 'Table', 'KPI'],
-    Components: ['Button', 'Filter', 'Current Selections', 'Search', 'Smart Heading', 'Login'],
-    Layout: ['Grid', 'Box', 'Sidebar', 'Modal'],
-    Hooks: ['useEngine', 'useHyperCube', 'useListObject', 'useSelectionObject', 'useSearch', 'useSidebar', 'useModal'],
-  },
+  docs: [
+    { type: 'category', label: 'Introduction', items: ['Introduction', 'Getting Started'], collapsed: false },
+    { type: 'category', label: 'Contexts', items: ['Motor'], collapsed: false },
+    { type: 'category', label: 'Visualisations', items: ['Column', 'Bar', 'Line', 'Pie', 'Table', 'KPI'], collapsed: false },
+    { type: 'category', label: 'Components', items: ['Button', 'Filter', 'Current Selections', 'Search', 'Smart Heading', 'Login'], collapsed: false },
+    { type: 'category', label: 'Layout', items: ['Grid', 'Box', 'Sidebar', 'Modal', 'NavItem'], collapsed: false },
+    { type: 'category', label: 'Hooks', items: ['useEngine', 'useHyperCube', 'useListObject', 'useSelectionObject', 'useSearch', 'useSidebar', 'useModal'], collapsed: false },
+  ]
 };
