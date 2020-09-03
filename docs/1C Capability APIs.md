6b2b01de85b6dd7e90417952993d8f33efbde817
lureilly1 <lureilly1@gmail.com>
2020-09-03 22:28:07 +0200

    cap apis

---
+++ docs/1C Capability APIs.md
@@ -0,0 +1,15 @@
+---
+id: Capability APIs
+title: Capability APIs
+sidebar_label: Capability APIs
+---
+
+Motor js also supports using the Capability APIs in your React mashups. These APIs will <b>only</b> be loaded
+into your mashup if you use one of the components that are built with these APIs (these are clearly marked in the sidebar).
+
+:::info
+By loading the Capability APIs into your mashup, you are also loading CSS files from the Qlik server. 
+Note that these may clash with your mashup CSS.
+:::
+
+The Capability APIs are a collection of java script APIs that enable you easily embed Qlik objects into your web page and also probides various methods for interacting with the engine. See the Qlik [docs](https://qlik.dev/apis/javascript/capabilities) for more information.
--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,10 @@
 module.exports = {
   docs: [
-    { type: 'category', label: 'Introduction', items: ['Introduction', 'Getting Started'], collapsed: false },
+    { type: 'category', label: 'Introduction', items: ['Introduction', 'Getting Started', 'Capability APIs'], collapsed: false },
     { type: 'category', label: 'Contexts', items: ['Motor'], collapsed: false },
-    { type: 'category', label: 'Visualisations', items: ['Column', 'Bar', 'Line', 'Pie', 'Table', 'KPI'], collapsed: false },
-    { type: 'category', label: 'Components', items: ['Button', 'Filter', 'Current Selections', 'Search', 'Smart Heading', 'Login'], collapsed: false },
+    { type: 'category', label: 'Visualisations', items: ['Column', 'Bar', 'Line', 'Pie', 'Table', 'KPI', 'Qlik Object'], collapsed: false },
+    { type: 'category', label: 'Components', items: ['Button', 'Filter', 'Current Selections', 'Qlik Selections', 'Search', 'Smart Heading', 'Login'], collapsed: false },
     { type: 'category', label: 'Layout', items: ['Grid', 'Box', 'Sidebar', 'Modal', 'NavItem'], collapsed: false },
-    { type: 'category', label: 'Hooks', items: ['useEngine', 'useHyperCube', 'useListObject', 'useSelectionObject', 'useSearch', 'useSidebar', 'useModal'], collapsed: false },
+    { type: 'category', label: 'Hooks', items: ['useEngine', 'useCapability','useHyperCube', 'useListObject', 'useSelectionObject', 'useSearch', 'useSidebar', 'useModal'], collapsed: false },
   ]
 };
