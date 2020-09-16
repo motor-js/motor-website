c89bce5af34ff05a680ae89244b3e4823e389c8f
lureilly1 <lureilly1@gmail.com>
2020-09-16 10:40:57 +0100

    Capability API docs

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: [
     { type: 'category', label: 'Introduction', items: ['Introduction', 'Getting Started', 'Capability APIs'], collapsed: false },
-    { type: 'category', label: 'Contexts', items: ['Motor'], collapsed: false },
+    { type: 'category', label: 'Contexts', items: ['Motor', 'Engine Context', 'Capability Context'], collapsed: false },
     { type: 'category', label: 'Visualisations', items: ['Column', 'Bar', 'Line', 'Pie', 'Table', 'KPI', 'Qlik Object'], collapsed: false },
     { type: 'category', label: 'Components', items: ['Button', 'Filter', 'Current Selections', 'Qlik Selections', 'Search', 'Smart Heading', 'Login'], collapsed: false },
     { type: 'category', label: 'Layout', items: ['Grid', 'Box', 'Sidebar', 'Modal', 'NavItem'], collapsed: false },
